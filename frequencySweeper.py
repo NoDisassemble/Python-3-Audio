fb53a8e6c7b423083af44645edb7252564998a6d
NoDisassemble <anything@dant35an0nym0u5.com>
2016-12-16 21:20:54 -0800

    Add files via upload

---
+++ frequencySweeper.py
@@ -0,0 +1,67 @@
+import winsound
+
+print('''
+___________                                                          _________
+\_   _____/______   ____  ________ __   ____   ____   ____ ___.__.  /   _____/_  _  __ ____   ____ ______   ___________
+ |    __) \_  __ \_/ __ \/ ____/  |  \_/ __ \ /    \_/ ___<   |  |  \_____  \\\ \/ \/ // __ \_/ __ \\\____ \_/ __ \_  __ \\
+ |     \   |  | \/\  ___< <_|  |  |  /\  ___/|   |  \  \___\___  |  /        \\\     /\  ___/\  ___/|  |_> >  ___/|  | \/
+ \___  /   |__|    \___  >__   |____/  \___  >___|  /\___  > ____| /_______  / \/\_/  \___  >\___  >   __/ \___  >__|
+     \/                \/   |__|           \/     \/     \/\/              \/             \/     \/|__|        \/
+
+---------------------------------------------------------------------------------------------------------------------------
+2016 v1.0
+by
+NoDisessemble.me
+---------------------------------------------------------------------------------------------------------------------------
+''')
+
+while True:
+    start=40
+    answer=input("Start Frequency Sweep? [Yes or No]: ")
+
+    if answer in ["Yes", "yes"]:
+        try:
+            increment = int(input("In What Increments? "))
+        except ValueError:
+            print("")
+            print("[!] Invalid Response:")
+            input("Press enter to try again:")
+            print("")
+            continue
+        else:
+            print("")
+            x = "-"
+            for i in range(1, 35):
+                print(x, end="")
+            print('')
+            print("[!] Generating Frequency Sweep [!]")
+            x = "-"
+            for i in range(1, 35):
+                print(x, end="")
+            print("")
+            while start < 32767:
+                winsound.Beep(start,1000)
+                print("[+}",start, "hz")
+                start = start +int(increment)
+            x = "-"
+            for i in range(1, 35):
+                print(x, end="")
+            print("")
+            print("[!] Frequency Sweep Completed: [!]")
+            x = "-"
+            for i in range(1, 35):
+                print(x, end="")
+            print("")
+            print("")
+            input("Press Enter to do another sweep:")
+            print("")
+            continue
+    if answer in ["No", "no"]:
+        print("")
+        print("[!] Exiting program...")
+        quit()
+    else:
+        print("")
+        print("[!] Invalid Response:")
+    input("Press enter to try again:")
+    print("")
---
+++ toneGenerator.py
@@ -0,0 +1,50 @@
+import winsound
+
+print('''
+___________                      ________                                   __
+\__    ___/___   ____   ____    /  _____/  ____   ____   ________________ _/  |_  ___________
+  |    | /  _ \ /    \_/ __ \  /   \  ____/ __ \ /    \_/ __ \_  __ \__  \\\   __\/  _ \_  __ \\
+  |    |(  <_> )   |  \  ___/  \    \_\  \  ___/|   |  \  ___/|  | \// __ \|  | (  <_> )  | \/
+  |____| \____/|___|  /\___  >  \______  /\___  >___|  /\___  >__|  (____  /__|  \____/|__|
+                    \/     \/          \/     \/     \/     \/           \/
+
+-----------------------------------------------------------------------------------------------------------------------------------
+2016 v1.0
+by
+NoDisessemble.me
+-----------------------------------------------------------------------------------------------------------------------------------
+''')
+
+while True:
+    try:
+        hertz=int(input("Choose a frequency in hertz: 37 - 32,767: "))
+    except ValueError:
+        print("")
+        print("Invalid entry:")
+        input("Press [Enter] to try again:")
+        print("")
+    else:
+        try:
+            duration=int(input("How many Seconds to play? "))
+            print("")
+        except ValueError:
+            print("")
+            print("Invalid entry:")
+            input("Press [Enter] to try again:")
+            print("")
+        else:
+            x = "-"
+            for i in range(1, 45):
+                print(x, end="")
+            print("")
+            print("[!] Generating", hertz,"hz for", duration, "Seconds [!]")
+            x = "-"
+            for i in range(1, 45):
+                print(x, end="")
+            print("")
+            winsound.Beep(int(hertz),int(duration*1000))
+            print("")
+            print("[+] Tone Complete:")
+            print("")
+            input("Press [Enter] to generate another tone:")
+            print("")

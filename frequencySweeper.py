6f06ab050c0f69a512781e36d7896d5d55ee1281
NoDisassemble <anything@dant35an0nym0u5.com>
2016-12-28 22:32:06 -0800

    Update frequencySweeper.py

--- frequencySweeper.py
+++ frequencySweeper.py
@@ -1,53 +1,78 @@
 import winsound
 
 print('''
-================================================================================================
-___________                      ________                                   __
-\__    ___/___   ____   ____    /  _____/  ____   ____   ________________ _/  |_  ___________
-  |    | /  _ \ /    \_/ __ \  /   \  ____/ __ \ /    \_/ __ \_  __ \__  \\\   __\/  _ \_  __ \\
-  |    |(  <_> )   |  \  ___/  \    \_\  \  ___/|   |  \  ___/|  | \// __ \|  | (  <_> )  | \/
-  |____| \____/|___|  /\___  >  \______  /\___  >___|  /\___  >__|  (____  /__|  \____/|__|
-                    \/     \/          \/     \/     \/     \/           \/
+===========================================================================================================================
+___________                                                          _________
+\_   _____/______   ____  ________ __   ____   ____   ____ ___.__.  /   _____/_  _  __ ____   ____ ______   ___________
+ |    __) \_  __ \_/ __ \/ ____/  |  \_/ __ \ /    \_/ ___<   |  |  \_____  \\\ \/ \/ // __ \_/ __ \\\____ \_/ __ \_  __ \\
+ |     \   |  | \/\  ___< <_|  |  |  /\  ___/|   |  \  \___\___  |  /        \\\     /\  ___/\  ___/|  |_> >  ___/|  | \/
+ \___  /   |__|    \___  >__   |____/  \___  >___|  /\___  > ____| /_______  / \/\_/  \___  >\___  >   __/ \___  >__|
+     \/                \/   |__|           \/     \/     \/\/              \/             \/     \/|__|        \/
 
--------------------------------------------------------------------------------------------------
-Description: Simple program that generates a tone from user input using the winsound.Beep module.
-Keywords: [Python 3, Tone Generator, Note Generator, Beep Generator, winsound.Beep]
+---------------------------------------------------------------------------------------------------------------------------
+Description: Simple program that sweeps a fixed frequency range from user input.
+Keywords: [Frequency Sweeper, Tone Sweeper, Sound Sweeper, Frequency Range]
 2016 v1.0
 by
 NoDisassemble.me
--------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
 ''')
 
 while True:
-    try:
-        hertz=int(input("Choose a frequency in hz: 37 - 32,767: "))
-    except ValueError:
-        print("")
-        print("Invalid entry:")
-        input("Press [Enter] to try again:")
-        print("")
-    else:
+    start=40
+    answer=input("Start Frequency Sweep? [Yes or No]: ")
+
+    if answer in ["Yes", "yes"]:
         try:
-            duration=int(input("How many Seconds to play? "))
-            print("")
+            increment = int(input("In What Increments? "))
         except ValueError:
             print("")
-            print("Invalid entry:")
-            input("Press [Enter] to try again:")
+            print("[!] Invalid Response:")
+            input("Press enter to try again:")
             print("")
+            continue
         else:
+            print("")
             x = "-"
-            for i in range(1, 41):
+            for i in range(1, 35):
+                print(x, end="")
+            print('')
+            print("[!] Generating Frequency Sweep [!]")
+            x = "-"
+            for i in range(1, 35):
                 print(x, end="")
             print("")
-            print("[!] Generating", hertz,"hz for", duration, "Seconds [!]")
+            while start < 32767:
+                winsound.Beep(start,1000)
+                print("[+}",start, "hz")
+                start = start +int(increment)
             x = "-"
-            for i in range(1, 41):
+            for i in range(1, 35):
                 print(x, end="")
             print("")
-            winsound.Beep(int(hertz),int(duration*1000))
+            print("[!] Frequency Sweep Completed: [!]")
+            x = "-"
+            for i in range(1, 35):
+                print(x, end="")
             print("")
-            print("[+] Tone Complete:")
             print("")
-            input("Press [Enter] to generate another tone:")
+            input("Press Enter to do another sweep:")
             print("")
+            continue
+    if answer in ["No", "no"]:
+        print("")
+        x = "-"
+        for i in range(1, 35):
+            print(x, end="")
+        print("")
+        print("[!] Exiting program...")
+        x = "-"
+        for i in range(1, 35):
+            print(x, end="")
+        print("")
+        quit()
+    else:
+        print("")
+        print("[!] Invalid Response:")
+    input("Press enter to try again:")
+    print("")
